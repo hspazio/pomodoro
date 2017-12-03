1e93558e1a2f08479af4c0c19368ba44bd6d0bc2
Fabio Pitino <pitinofabio@gmail.com>
2017-12-03 12:54:01 +0000

    add break suggestions

--- pomodoro.go
+++ pomodoro.go
@@ -12,14 +12,11 @@ import (
 	"github.com/0xAX/notificator"
 )
 
-// NOTE:
-// pmd > starts timer. Also can take parameters to override default timeout
-// ctrl-c to stop  and capture signal for clean shutdown
 type results map[string]int
 
 const (
 	resultsFile = "pomodoro.dat"
-	timeout     = 2
+	timeout     = 25
 )
 
 var notify *notificator.Notificator
@@ -28,21 +25,21 @@ func main() {
 	var graph = flag.Bool("graph", false, "show results graph")
 	flag.Parse()
 
-	res, err := load()
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+	var res results
+	if err := load(&res); err != nil {
+		exit(err)
 	}
 
 	if *graph {
 		for date, count := range res {
 			fmt.Printf("%s: %s %d\n", date, strings.Repeat("#", count), count)
 		}
-		os.Exit(0)
+		exit(nil)
 	}
 
-	tick := time.Tick(1 * time.Second)
-	alarm := time.After(timeout * time.Second)
+	tick := time.Tick(1 * time.Minute)
+	alarm := time.After(timeout * time.Minute)
+	notify := notificator.New(notificator.Options{AppName: "Pomodoro"})
 
 L:
 	for {
@@ -52,41 +49,36 @@ L:
 		case <-alarm:
 			res[today()]++
 			message := fmt.Sprintf("%d cycles today", res[today()])
-			fmt.Printf("\nPomodoro! %s", message)
+			var suggestion string
+			if res[today()]%4 == 0 {
+				suggestion = "take a longer break"
+			} else {
+				suggestion = "rest for 5 minutes"
+			}
 
-			notify = notificator.New(notificator.Options{
-				AppName: "Pomodoro",
-			})
-
-			err := notify.Push(message, "", "", notificator.UR_NORMAL)
+			fmt.Printf("\nPomodoro! %s, %s", message, suggestion)
+			err := notify.Push(message, suggestion, "", notificator.UR_NORMAL)
 			if err != nil {
-				fmt.Println(err)
-				os.Exit(1)
+				exit(err)
 			}
 
 			if err = save(res); err != nil {
-				fmt.Println(err)
-				os.Exit(1)
+				exit(err)
 			}
 			break L
 		}
 	}
 }
 
-func today() string {
-	return time.Now().Format("2006-01-02")
-}
-
-func load() (results, error) {
-	results := make(map[string]int)
+func load(r *results) error {
 	b, err := ioutil.ReadFile(resultsFile)
 	if err == nil {
-		if err := json.Unmarshal(b, &results); err != nil {
-			return nil, fmt.Errorf("file %s does not contain a valid json", resultsFile)
+		if err := json.Unmarshal(b, r); err != nil {
+			return fmt.Errorf("file %s does not contain a valid json", resultsFile)
 		}
 	}
 
-	return results, nil
+	return nil
 }
 
 func save(r results) error {
@@ -100,4 +92,17 @@ func save(r results) error {
 	}
 
 	return nil
+}
+
+func today() string {
+	return time.Now().Format("2006-01-02")
+}
+
+func exit(err error) {
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	os.Exit(0)
 }
